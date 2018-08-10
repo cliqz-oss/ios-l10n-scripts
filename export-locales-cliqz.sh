107245c279beb0eac44820dffd5c77637e3db561
Mahmoud Adam <mahmoud@cliqz.com>
2018-08-10 11:33:56 +0200

    Created scripts to export localized strings for cliqz product

---
+++ export-locales-cliqz.sh
@@ -0,0 +1,115 @@
+#! /usr/bin/env bash
+
+#
+# Assumes the following is installed:
+#
+#  git
+#  brew
+#  python via brew
+#  virtualenv via pip in brew
+#
+# Syntax is:
+# ./export-locales-cliqz.sh (name of project file) (name of l10n repo) (name of xliff file) [clean]
+#
+# We can probably check all that for the sake of running this hands-free
+# in an automated manner.
+#
+
+clean_run=false
+if [ $# -ge 3 ]
+then
+    if [ $# -eq 4 ]
+    then
+        if [ "$4" == "clean" ]
+        then
+            clean_run=true
+        else
+            echo "Unknown parameter: $4"
+            echo "Leave empty to reuse an existing venv, use 'clean' to create a new one"
+            exit 1
+        fi
+    fi
+    xcodeproj="$1"
+    l10n_repo="$2"
+    l10n_file="$3"
+else
+    echo "Not enough parameters."
+    echo "Syntax: ./export-locales.sh (name of .xcodeproj file) (name of l10n repo) (name of xliff file) [clean]"
+    echo "Example: ./export-locales.sh Client.xcodeproj firefoxios-l10n firefox-ios.xliff clean"
+    echo "You should call this script via wrappers like export-locales-firefox.sh"
+    exit 1
+fi
+
+if [ ! -d ${xcodeproj} ]
+then
+  echo "Please run this from the project root that contains ${xcodeproj}"
+  exit 1
+fi
+
+if [ -d ${l10n_repo} ]
+then
+  echo "There already is a ${l10n_repo} checkout. Aborting to let you decide what to do."
+  exit 1
+fi
+
+SDK_PATH=`xcrun --show-sdk-path`
+
+SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# If the virtualenv with the Python modules that we need doesn't exist,
+# or a clean run was requested, create the virtualenv.
+if [ ! -d export-locales-env ] || [ "${clean_run}" = true ]
+then
+    rm -rf export-locales-env || exit 1
+    echo "Setting up new virtualenv..."
+    virtualenv export-locales-env --python=python2.7 || exit 1
+    source export-locales-env/bin/activate || exit 1
+    # install libxml2
+    CFLAGS=-I"$SDK_PATH/usr/include/libxml2" LIBXML2_VERSION=2.9.2 pip install lxml || exit 1
+else
+    echo "Reusing existing virtualenv found in export-locales-env"
+    source export-locales-env/bin/activate || exit 1
+fi
+
+# Check out a clean copy of the l10n repo
+git clone https://github.com/cliqz-oss/${l10n_repo} || exit 1
+
+# Export English base to /tmp/en.xliff
+rm -f /tmp/en.xliff || exit 1
+echo "Exporting en-US with xcodebuild"
+xcodebuild -exportLocalizations -localizationPath /tmp -project ${xcodeproj} -exportLanguage en || exit 1
+
+if [ ! -f /tmp/en.xliff ]
+then
+  echo "Export failed. No /tmp/en.xliff generated."
+  exit 1
+fi
+
+# Create a branch in the repository
+cd ${l10n_repo}
+branch_name=$(date +"%Y%m%d_%H%M")
+git branch ${branch_name}
+git checkout ${branch_name}
+
+# Copy the English XLIFF file into the repository and commit
+cp /tmp/en.xliff en-US/${l10n_file} || exit 1
+
+# cleanup English locale
+${SCRIPTS}/xliff-cliqz-cleanup.py ../${l10n_repo}/en-US/*.xliff || exit 1
+
+# Update all locales
+${SCRIPTS}/update-xliff.py . ${l10n_file} || exit 1
+
+
+git commit -a -m "Updated localized files"
+
+
+echo
+echo "NOTE"
+echo "NOTE Use the following command to push the branch to Github where"
+echo "NOTE you can create a Pull Request:"
+echo "NOTE"
+echo "NOTE   cd ${l10n_repo}"
+echo "NOTE   git push --set-upstream origin $branch_name"
+echo "NOTE"
+echo
---
+++ xliff-cliqz-cleanup.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+#
+# xliff-cliqz-cleanup.py <files>
+#
+#  1. Remove all <file> sections that we do not care about. We only care
+#     about the one for our main app and those for our extensions.
+#
+#  2. Look at all remaining <file> sections and remove those strings that
+#     should not be localized. Currently that means: CFBundleDisplayName,
+#     CFBundleName and CFBundleShortVersionString.
+#
+#  3. Copy English in required strings.
+#
+#  4. Remove all remaining <file> sections that now have no <trans-unit>
+#     nodes in their <body> anymore.
+#
+# Modifies files in place. Makes no backup.
+#
+
+import sys
+
+from lxml import etree
+
+NS = {'x':'urn:oasis:names:tc:xliff:document:1.2'}
+
+def indent(elem, level=0):
+    # Prettify XML output
+    # http://effbot.org/zone/element-lib.htm#prettyprint
+    i = '\n' + level*'  '
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + '  '
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+files_to_keep = [
+    'Client/en.lproj/InfoPlist.strings',
+    'Cliqz/Resources/en.lproj/Cliqz.stringsdict',
+    'Cliqz/Resources/en.lproj/Cliqz.strings'
+]
+
+required_ids = [
+    'NSCameraUsageDescription',
+    'NSLocationWhenInUseUsageDescription',
+    'NSMicrophoneUsageDescription',
+    'NSPhotoLibraryAddUsageDescription',
+    'NSPhotoLibraryUsageDescription',
+    'NSFaceIDUsageDescription',
+    'ShortcutItemTitleNewPrivateTab',
+    'ShortcutItemTitleNewTab',
+    'ShortcutItemTitleQRCode',
+]
+
+if __name__ == '__main__':
+    for path in sys.argv[1:]:
+        # Read it in and modify it in memory
+        with open(path) as fp:
+            try:
+                tree = etree.parse(fp)
+                root = tree.getroot()
+            except Exception as e:
+                print("ERROR: Can't parse file %s" % path)
+                print(e)
+                continue
+
+            # 1. Remove sections we do not care about
+            for file_node in root.xpath('//x:file', namespaces=NS):
+                file_name = file_node.get('original')
+                if file_name not in files_to_keep:
+                    file_node.getparent().remove(file_node)
+                    
+
+            for trans_node in root.xpath('//x:trans-unit', namespaces=NS):
+                file_name = trans_node.getparent().getparent().get('original')
+                source_string = trans_node.xpath('./x:source', namespaces=NS)[0].text
+                original_id = trans_node.get('id')
+
+                # 2. Copy English in required strings if there's no translation
+                if original_id in required_ids and len(trans_node.xpath('./x:target', namespaces=NS)) == 0:
+                    child = etree.Element('target')
+                    child.text = source_string
+                    trans_node.insert(1, child)
+
+            # 3. Remove empty file sections
+            for file_node in root.xpath('//x:file', namespaces=NS):
+                file_name = file_node.get('original')
+                if file_name.endswith('Info.plist'):
+                    trans_unit_nodes = file_node.xpath('x:body/x:trans-unit', namespaces=NS)
+                    if len(trans_unit_nodes) == 0:
+                        file_node.getparent().remove(file_node)
+        # Write it back to the same file
+        with open(path, 'w') as fp:
+            indent(root)
+            xliff_content = etree.tostring(
+                                tree,
+                                encoding='UTF-8',
+                                xml_declaration=True,
+                                pretty_print=True
+                            )
+            fp.write(xliff_content)
